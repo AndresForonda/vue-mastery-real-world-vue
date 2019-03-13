894bd9720eaf8621979ec53b92a6d8688e984b0d
Andres Fernando Foronda Espinal <andresforonda.af@gmail.com>
2019-03-13 15:06:29 -0300

    Adding params to routes

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
     <h1>Events Listing</h1>
+    <router-link :to="{ name: 'event-show', params: { id: 43 } }">
+      Show Event
+    </router-link>
   </div>
 </template>
--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
-    <h1>Event details #1</h1>
+    <h1>Event details {{ id }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  props: ['id']
+}
+</script>
