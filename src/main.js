57682b42df61ee18bf7bf8c5064274d90e72bedd
Andres Fernando Foronda Espinal <andresforonda.af@gmail.com>
2019-03-13 23:09:42 -0300

    Ending real-world-vue

--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+Vue.config.productionTip = false
+const requireComponent = require.context(
+  './components', // The relative path of the directory to search
+  false, // subdirectories will not be searched
+  /Base[A-Z]\w+\.(vue|js)$/ // Regular expresión that searches for components starting with "Base" and ending with .vue or .js
+)
 
-Vue.config.productionTip = false;
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+  const componentName = upperFirst(
+    camelCase(
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1') // removes what's before and after the filename itself
+    )
+  )
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
