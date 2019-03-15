672a5682e00a9955aa0baa1e242b2d5c7273e790
Andres Fernando Foronda Espinal <andresforonda.af@gmail.com>
2019-03-15 10:58:45 -0300

    Basic vuex settings

--- src/store.js
+++ src/store.js
@@ -1,10 +1,33 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
+  state: {
+    user: {
+      id: 'abc123',
+      name: 'Andres Foronda'
+    },
+    categories: [
+      'sustainaility',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [{ id: 1, name: 'Hello' }, { id: 2, name: 'World' }]
+  },
   mutations: {},
-  actions: {}
-});
+  actions: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
+})
