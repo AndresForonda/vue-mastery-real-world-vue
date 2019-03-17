805276cadafb248cf95d66e23ba35b8282a97b37
Andres Fernando Foronda Espinal <andresforonda.af@gmail.com>
2019-03-16 21:20:36 -0300

    Basic vuex module store setting

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -10,25 +10,37 @@
       <h3>Name & describe your event</h3>
       <div class="field">
         <label>Title</label>
-        <input v-model="event.title" type="text" placeholder="Add an event title"/>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="Add an event title"
+        />
       </div>
 
       <div class="field">
         <label>Description</label>
-        <input v-model="event.description" type="text" placeholder="Add a description"/>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="Add a description"
+        />
       </div>
 
       <h3>Where is your event?</h3>
       <div class="field">
         <label>Location</label>
-        <input v-model="event.location" type="text" placeholder="Add a location"/>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="Add a location"
+        />
       </div>
 
       <h3>When is your event?</h3>
 
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date"/>
+        <datepicker v-model="event.date" placeholder="Select a date" />
       </div>
 
       <div class="field">
@@ -38,15 +50,58 @@
         </select>
       </div>
 
-      <input type="submit" class="button -fill-gradient" value="Submit"/>
+      <input type="submit" class="button -fill-gradient" value="Submit" />
     </form>
   </div>
 </template>
 
 <script>
-import DatePicker from 'vuejs-datepicker'
-import { mapState, mapGetters } from 'vuex'
+import Datepicker from 'vuejs-datepicker'
 export default {
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 0; i < 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject()
+    }
+  },
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch('createEvent', this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
+        })
+        .catch(() => {
+          console.lot('There was an error creating your event')
+        })
+    },
+    createFreshEventObject() {
+      const user = this.$store.state.user.user
+      const id = Math.floor(Math.random() * 100000000)
+      return {
+        id: id,
+        user: user,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
+  }
 }
 </script>
 
@@ -54,3 +109,4 @@ export default {
 .field {
   margin-bottom: 24px;
 }
+</style>
