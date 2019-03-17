ea6537f7143d2c32b3e3f34a613c3ddc2477711a
Andres Fernando Foronda Espinal <andresforonda.af@gmail.com>
2019-03-17 19:14:16 -0300

    Network and 404 error pages added

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import EventCreate from './views/EventCreate.vue'
 import EventList from './views/EventList.vue'
 import EventShow from './views/EventShow.vue'
+import NetworkIssue from './views/NetworkIssue.vue'
+import NotFound from './views/NotFound.vue'
 import NProgress from 'nprogress'
 import store from '@/store/store'
 
@@ -12,6 +14,17 @@ const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    {
+      path: '/404',
+      name: '404',
+      component: NotFound,
+      props: true
+    },
+    {
+      path: 'network-issue',
+      name: 'network-issue',
+      component: NetworkIssue
+    },
     {
       path: '/',
       name: 'event-list',
@@ -24,16 +37,29 @@ const router = new Router({
       component: EventShow,
       props: true,
       beforeEnter(routeTo, routeFrom, next) {
-        store.dispatch('event/fetchEvent', routeTo.params.id).then(event => {
-          routeTo.params.event = event
-          next()
-        })
+        store
+          .dispatch('event/fetchEvent', routeTo.params.id)
+          .then(event => {
+            routeTo.params.event = event
+            next()
+          })
+          .catch(error => {
+            if (error.response && error.response.status == '404') {
+              next({ name: '404', params: { resource: 'event' } })
+            } else {
+              next({ name: 'network-issue' })
+            }
+          })
       }
     },
     {
       path: '/event/create',
       name: 'event-create',
       component: EventCreate
+    },
+    {
+      path: '*',
+      redirect: { name: '404', params: { resource: 'page' } }
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>
+      The {{ resource ? resource : 'page' }} you're looking for is not here.
+    </h1>
+    <router-link :to="{ name: 'event-list' }">
+      Back to the home page.
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    resource: {
+      type: String
+    }
+  }
+}
+</script>
